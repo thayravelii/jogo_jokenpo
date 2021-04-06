12474af8511da5f49d9a21616dba9af65118f7d7
thayravelii <69326825+thayravelii@users.noreply.github.com>
2021-04-06 10:52:18 -0300

    Update script.py

--- script.py
+++ script.py
@@ -6,15 +6,34 @@ dicionario = {'R': 'ROCK', 'P': 'PAPER', 'S': 'SCISSORS'}
 lista = ['R', 'P', 'S']
 score_maquina = 0
 score_jogador = 0
-jogar = 'C'
-maquina = dicionario[lista[random.randint(0, 2)]].upper().strip()[0]
+continuar = 'C'
+escolha_maquina = dicionario[lista[random.randint(0, 2)]].upper().strip()[0]
 
-while jogar == 'C':
+
+def showResult(result, score):
+    if result == 0:
+        print('EMPATE!')
+    elif result == 1:
+        print('GANHOU!')
+        score = score + 1
+    elif result == 2:
+        print('PERDEU!')
+        score = score + 1
+    return score
+
+def showPlacar():
+    print('===============Placar===============')
+    print('Jogador:{}'.format(score_jogador))
+    print('Máquina:{}'.format(score_maquina))
+    print('='*36)
+  
+
+while continuar == 'C':
     print('='*40)
     print(emojize('''Para começar, escolha sua jogada:
         [R]ROCK :fist:
         [P]PAPER :hand:
-        [S]SCISSORS :v:''', use_aliases= True))
+        [S]SCISSORS :v:''', use_aliases=True))
     print('='*40)
     jogador = input('Qual é a sua jogada?').upper().strip()[0]
     print('='*40)
@@ -25,73 +44,52 @@ while jogar == 'C':
     print('PÔ...')
     sleep(1)
     print('='*40)
-    print('A jogada do seu adversario foi: {}'.format(dicionario[maquina]))
+    print('A jogada do seu adversario foi: {}'.format(
+        dicionario[escolha_maquina]))
     print('Sua jogada:{}'.format(dicionario[jogador]))
-
+#PEDRA
     if jogador == "R":
-        if maquina == "R":
-            print('EMPATE!')
-            print('===============Placar===============')
-            print('Jogador:{}'.format(score_jogador))
-            print('Máquina:{}'.format(score_maquina))
-            print('='*36)
-        elif maquina == "P":
-            print('VOCÊ GANHOU!')
-            score_jogador = score_jogador + 1
-            print('===============Placar===============')
-            print('Jogador:{}'.format(score_jogador))
-            print('Máquina:{}'.format(score_maquina))
-            print('='*36)
-        elif maquina == "S":
-            print('VOCÊ PERDEU!')
-            score_maquina = score_maquina + 1
-            print('===============Placar===============')
-            print('Jogador:{}'.format(score_jogador))
-            print('Máquina:{}'.format(score_maquina))
-            print('='*36)
+        if escolha_maquina == "R":   
+            showResult(0, 0)
+            showPlacar()
+        elif escolha_maquina == "P":
 
+            score_maquina = showResult(2, score_maquina)
+            showPlacar()
+
+        elif escolha_maquina == "S":
+            
+            score_jogador = showResult(1, score_jogador)
+            showPlacar()
+#PAPEL
     elif jogador == "P":
-        if maquina == "R":
-            print('VOCÊ GANHOU!')
-            score_jogador = score_jogador + 1
-            print('===============Placar===============')
-            print('Jogador:{}'.format(score_jogador))
-            print('Máquina:{}'.format(score_maquina))
-            print('='*36)
-        elif maquina == "P":
-            print("EMPATE!")
-            print('===============Placar===============')
-            print('Jogador:{}'.format(score_jogador))
-            print('Máquina:{}'.format(score_maquina))
-            print('='*36)
-        elif maquina == "S":
-            print('VOCÊ PERDEU!')
-            score_maquina = score_maquina + 1
-            print('===============Placar===============')
-            print('Jogador:{}'.format(score_jogador))
-            print('Máquina:{}'.format(score_maquina))
-            print('='*36)
+        if escolha_maquina == "R":
+
+            score_jogador = showResult(1, score_jogador)
+            showPlacar()
+
+        elif escolha_maquina == "P":
+            showResult(0, 0)    
+            showPlacar()
+
+        elif escolha_maquina == "S":
+            score_maquina = showResult(2, score_maquina)
+            showPlacar()
+#TESOURA
     elif jogador == "S":
-        if maquina == "R":
-            print("VOCÊ PERDEU!")
-            score_maquina = score_maquina + 1
-            print('===============Placar===============')
-            print("Jogador:{}".format(score_jogador))
-            print("Máquina:{}".format(score_maquina))
-            print('='*36)
-        elif maquina == "P":
-            print("VOCÊ GANHOU!")
-            score_jogador = score_jogador + 1
-            print('===============Placar===============')
-            print("Jogador:{}".format(score_jogador))
-            print("Máquina:{}".format(score_maquina))
-            print('='*36)
-        elif maquina == "S":
-            print("EMPATE!")
-            print('===============Placar===============')
-            print("Jogador:{}".format(score_jogador))
-            print("Máquina:{}".format(score_maquina))
-            print('='*36)
+        if escolha_maquina == "R":
+
+            score_maquina = showResult(2, score_maquina)
+            showPlacar()
+
+        elif escolha_maquina == "P":
+            score_jogador = showResult(1, score_jogador)
+            showPlacar()
+
+        elif escolha_maquina == "S":
+            showResult(0, 0)
+            showPlacar()
+    
 
     print('===============FIM DA RODADA===============')
     jogar = input('''
@@ -99,7 +97,7 @@ while jogar == 'C':
     [S] STOP
     DESEJA CONTINUAR JOGANDO?''').upper().strip()[0]
     print('='*30)
-    
+
     if jogar == "S":
         print('FIM DE JOGO!!!')
         print('===============PLACAR FINAL===============')
